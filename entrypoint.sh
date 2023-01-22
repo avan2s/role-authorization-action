25c14eca6950a48c335bf102926969d50c224c72
Andreas van Helden <andreas-van-helden@gmx.de>
2023-01-22 01:50:02 +0100

    print permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ USERNAME=$(jq -r '.actor.login' ${{toJson(github.event)}})
 PERMISSIONS=$(curl -H "Authorization: token ${{secrets.GITHUB_TOKEN}}" -s https://api.github.com/repos/${{REPO_NAME}}/collaborators/${{USERNAME}} | jq -r 'to_entries|map("\(.key)=\(.value)")|.[]')
 
 # Set output variables
+echo "Permissions are $PERMISSIONS"
 echo "::set-output name=permissions::$PERMISSIONS"
 echo "::set-output name=is_maintainer::$(echo $PERMISSIONS | grep -q maintain=true && echo true || echo false)"
 echo "::set-output name=is_admin::$(echo $PERMISSIONS | grep -q admin=true && echo true || echo false)"
