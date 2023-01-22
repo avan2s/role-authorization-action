5bfad21a2d10515e30352b594613609f484dac32
Andreas van Helden <andreas-van-helden@gmx.de>
2023-01-23 00:22:18 +0100

    fix pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,27 +12,27 @@ echo "GITHUB_ACTOR: $GITHUB_ACTOR"
 IS_MAINTAINER=$(curl -s -H "Authorization: token $github_token" https://api.github.com/repos/$OWNER/$REPO/collaborators/$GITHUB_ACTOR/permission | jq -r .permission)
 if [ "$IS_MAINTAINER" = "admin" ] || [ "$IS_MAINTAINER" = "write" ]; then
     echo "User is a maintainer"
-    echo "is_maintainer=true" > $GITHUB_ENV/is_maintainer
+    echo "is_maintainer=true" >> $GITHUB_OUTPUT
 else
     echo "User is not a maintainer"
-    echo "is_maintainer=false" > $GITHUB_ENV/is_maintainer
+    echo "is_maintainer=false" >> $GITHUB_OUTPUT
 fi
 
 # Check if the user is an admin
 IS_ADMIN=$(curl -s -H "Authorization: token $github_token" https://api.github.com/repos/$OWNER/$REPO/collaborators/$GITHUB_ACTOR/permission | jq -r .permission)
 if [ "$IS_ADMIN" = "admin" ]; then
     echo "User is an admin"
-    echo "is_admin=true" > $GITHUB_ENV/is_admin
+    echo "is_admin=true" >> $GITHUB_OUTPUT
 else
     echo "User is not an admin"
-    echo "is_admin=false" > $GITHUB_ENV/is_admin
+    echo "is_admin=false" >> $GITHUB_OUTPUT
 fi
 
 # Check if the user is the owner
 if [ "$OWNER" = "$GITHUB_ACTOR" ]; then
     echo "User is the owner"
-    echo "is_owner=true" > $GITHUB_ENV/is_owner
+    echo "is_owner=true" >> $GITHUB_OUTPUT
 else
     echo "User is not the owner"
-    echo "is_owner=false" > $GITHUB_ENV/is_owner
+    echo "is_owner=false" >> $GITHUB_OUTPUT
 fi
