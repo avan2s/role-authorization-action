fee05c3c2f1484a1271b4b451cc4ec1ca36b6cb5
Andreas van Helden <andreas-van-helden@gmx.de>
2023-01-23 00:57:34 +0100

    fix pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ echo $REPO
 # fi
 
 echo "make call"
-curl -w "%{http_code}\n" -s -H "Authorization: token $github_token" https://api.github.com/repos/$OWNER/$REPO/collaborators/$GITHUB_ACTOR/permission '.permissions.maintain'
+curl -w "%{http_code}\n" -s -H "Authorization: token $github_token" https://api.github.com/repos/$OWNER/$REPO/collaborators/$GITHUB_ACTOR/permission | jq '.permissions'
 
 echo "check roles..."
 
