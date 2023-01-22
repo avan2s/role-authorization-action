f94dabb4253579e2eba739e42da43c602c630e50
Andreas van Helden <andreas-van-helden@gmx.de>
2023-01-22 01:27:30 +0100

    implement pipeline

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM alpine:latest
+
+RUN apk update && \
+    apk add --no-cache \
+        curl \
+        jq
+
+COPY entrypoint.sh /entrypoint.sh
+
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+REPO_NAME=$(jq -r '.repository.name' ${{toJson(github.event)}})
+USERNAME=$(jq -r '.actor.login' ${{toJson(github.event)}})
+
+# Get user permissions
+PERMISSIONS=$(curl -H "Authorization: token ${{secrets.GITHUB_TOKEN}}" -s https://api.github.com/repos/${{REPO_NAME}}/collaborators/${{USERNAME}} | jq -r 'to_entries|map("\(.key)=\(.value)")|.[]')
+
+# Set output variables
+echo "::set-output name=permissions::$PERMISSIONS"
+echo "::set-output name=is_maintainer::$(echo $PERMISSIONS | grep -q maintain=true && echo true || echo false)"
+echo "::set-output name=is_admin::$(echo $PERMISSIONS | grep -q admin=true && echo true || echo false)"
+
+# Check if user is the owner
+OWNER=$(curl -H "Authorization: token ${{secrets.GITHUB_TOKEN}}" -s https://api.github.com/repos/${{REPO_NAME}} | jq -r '.owner.login')
+echo "::set-output name=is_owner::$OWNER"
